438e85e6638881bb105baf14801ef82124160a77
Juanpi <94568912+ssuanpi@users.noreply.github.com>
2024-04-01 18:24:21 +0200

    Update isPalindrome.py
    added seccond way on palindrome, more optimized using [::-1] to reverse a string.

--- isPalindrome.py
+++ isPalindrome.py
@@ -19,3 +19,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+"""
+def isPalindrome(x):
+    x_str = str(x)
+    if x_str == x_str[::-1]:
+        return True
+    else:
+        return False
+"""
